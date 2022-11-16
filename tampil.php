33c232abba14966c9bb59c39c1cda938e3e67249
feisawidya <109189909+feisawidya@users.noreply.github.com>
2022-11-16 14:56:55 +0700

    Add files via upload

--- tampil.php
+++ tampil.php
@@ -6,6 +6,18 @@ include("koneksi.php");
 <head>
 </head>
 <body>
+<?php
+    session_start();
+    if($_SESSION['status']!="login"){
+        header("location:../index.php?pesan=belum_login");
+    }
+    ?>
+
+    <h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
+
+    <br/>
+    <br/>
+
     <h1> PEMERIKSAAN HEWAN RS SUMBER WARAS</h1>
 
 
@@ -34,10 +46,11 @@ include("koneksi.php");
             echo "<a href='hapus.php?id=".$hewan['id']."'>Hapus</a> |";
             echo "</td>";
             echo "</tr>";
-            
-
         }
         ?>
+        <input type="button" value="Back" onclick="history.back(-1)"/>
+        <a href="tambah.php"><input type="button" name="tambah" value="Tambah Data"/></a>
         </table>
+        <a href="logout.php">LOGOUT</a>
     </body>
     </html>
